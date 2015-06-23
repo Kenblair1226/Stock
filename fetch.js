73236d3d81e50287c5a8e179f7c91ce5dcf526a8
Kenblair1226 <blair1226@gmail.com>
2015-06-23 17:49:06 +0800

    Introduce async.js to improve efficiency

--- fetch.js
+++ fetch.js
@@ -2,6 +2,7 @@
 var url = require('urllib');
 var Promise = require('promise');
 var Parse = require('parse').Parse;
+var async = require('async');
 
 var TWSE_HOST = 'http://www.twse.com.tw';
 var Stock = Parse.Object.extend('TWStocks');
@@ -34,7 +35,7 @@ function updateMonthly(stock_no, latest_date, today) {
 	            //ex: http://www.twse.com.tw/ch/trading/exchange/STOCK_DAY/STOCK_DAY_print.php?
 	            //    genpage=genpage/Report201505/201505_F3_1_8_2330.php&type=csv
 				
-		console.log('fetching ' + monStock);
+		//console.log('fetching ' + monStock);
 		url.request(monStock, {timeout:600000})
 		.then(function(result) {
 			var parsed_data = to_list(result.data.toString());
@@ -90,85 +91,87 @@ function to_list(data){
 		var date = new Date(temp[0]);
 		date.setYear(year);
 		item.t = date;
-		item.a = parseFloat(temp[1].replace(/"/g, '').replace(/,/g,'')); 	// 成交量
-		item.v = parseFloat(temp[2].replace(/"/g, '').replace(/,/g,'')); 	// 成交值
-		item.o = parseFloat(temp[3].replace(/,/g,'')); 						// 開盤價
-		item.h = parseFloat(temp[4].replace(/,/g,'')); 						// 最高價
-		item.l = parseFloat(temp[5].replace(/,/g,'')); 						// 最低價
-		item.c = parseFloat(temp[6].replace(/,/g,'')); 						// 收盤價
+		item.a = parseFloat(temp[1].replace(/"/g, '').replace(/,/g, '')); 	// 成交量
+		item.v = parseFloat(temp[2].replace(/"/g, '').replace(/,/g, '')); 	// 成交值
+		item.o = parseFloat(temp[3].replace(/,/g, ''));						// 開盤價
+		item.h = parseFloat(temp[4].replace(/,/g, ''));						// 最高價
+		item.l = parseFloat(temp[5].replace(/,/g, ''));						// 最低價
+		item.c = parseFloat(temp[6].replace(/,/g, ''));						// 收盤價
 		item.p = temp[7]; 													// 價差百分比
-		item.n = parseFloat(temp[8].replace(/,/g,'')); 						// 成交筆數
+		item.n = parseFloat(temp[8].replace(/"/g, '').replace(/,/g, '')); 	// 成交筆數
 		list.push(item);
 	}
 	return list.sort().reverse();
 }
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
 function Fetch(stock_no) {
-	var query = new Parse.Query(Stock);
-	query.equalTo('stock_no', stock_no);
-	query.descending('t').first().then(function(latest) {
-		if( latest === undefined)
-		{
-			// get data up to 10 years
-			var latest_date = new Date('2004/12/31');;
-		} else {
-			var latest_date = latest.get('t');
-		}
-		var today = new Date();
-		today.setHours(0,0,0,0);
-		console.log('today:\t' + today + '\nlatest:\t' + latest_date);
-		if(	today.getTime() === latest_date.getTime())
-		{
-			console.log('Already has the up-to-date data...');
-			return;
-		}
-		
-		// var promises = [];
-		// // ugly while loop to add 1 promise per second
-		// while(today > latest_date)
-		// {
-		// 	promises.push(updateMonthly(stock_no, latest_date, today));
-		// 	today.setMonth(today.getMonth()-1);
-		// }
-		// Promise.all(promises)
-		// .then( function(ret_arr) {
-		// 	var total = 0;
-		// 	for(var i = 0 ; i < ret_arr.length ; i++)
-		// 	{
-		// 		total += ret_arr[i];
-		// 	}
-		// 	console.log(total + ' records were added.');
-		// }, function(e) {
-		// 	console.error(e.toString());
-		// });
-		
-		for(var i = 0 ; today > latest_date ; i++)
-		{
-			arrangeTasks(i, stock_no, new Date(latest_date), new Date(today));
-			today.setMonth(today.getMonth()-1);
-		}
+	return new Promise( function(resolve, reject) {
+		var query = new Parse.Query(Stock);
+		query.equalTo('stock_no', stock_no);
+		query.descending('t').first().then(function(latest) {
+			if( latest === undefined)
+			{
+				// get data up to 10 years
+				var latest_date = new Date('2004/12/31');;
+			} else {
+				var latest_date = latest.get('t');
+			}
+			var today = new Date();
+			today.setHours(0,0,0,0);
+			//console.log('today:\t' + today + '\nlatest:\t' + latest_date);
+			if(	today.getTime() === latest_date.getTime())
+			{
+				console.log('Already has the up-to-date data...');
+				return;
+			}
+			
+			var arr = [];
+			for(var i = 0 ; today > latest_date ; i++)
+			{
+				arr.push(dummy(stock_no, latest_date, new Date(today.getTime())));
+				arr.push(sleep(1000));
+				today.setMonth(today.getMonth()-1);
+			}
+			async.series(arr, function(err, results) {
+				if(err) {
+					reject("async error: " + err.toString());
+					return;
+				}
+				var total = 0;
+				for(var i = 0 ; i < results.length ; i++)
+				{
+					total += results[i];
+				}
+				resolve(total);
+			});
+		});
 	});
 }
 
-function arrangeTasks(i, stock_no, latest_date, today)
-{
-	setTimeout( function() {
+function sleep(milliseconds) {
+	return function(callback) {
+		var start = new Date().getTime();
+		for (var i = 0; i < 1e7; i++) {
+			if ((new Date().getTime() - start) > milliseconds){
+				break;
+			}
+		}
+		callback(null, 0);
+	}
+}
+
+// dummy function to avoid function-in-a-loop (ES5 restriction)
+function dummy(stock_no, latest_date, today) {
+	return function(callback) {
 		updateMonthly(stock_no, latest_date, today)
-		.then( function(ret) {
-			console.log(ret + ' records were added.');
-		}, function(e) {
-			console.error(e.toString());
-		});
-	}, 1000*i);
+			.then( function(ret) {
+				console.log(ret + ' records were added for ' + stock_no + 
+					" for " + today.getFullYear() + "/" + parseInt(today.getMonth()+1));
+				callback(null, ret);
+			}, function(e) {
+				callback(e.toString());
+			});
+	}
 }
 
 module.exports.Fetch = Fetch;
--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 var Fetch = require('./fetch.js').Fetch;
 var Parse = require('parse').Parse;
+var async = require('async');
 
-
+Parse.initialize("YOUR_APP_ID", "YOUR_JAVASCRIPT_KEY");
 
 var Stock_list = Parse.Object.extend('TWSE_list_20150610');
 var query = new Parse.Query(Stock_list);
+var func_list = [];
 query.limit(1000);
 query.ascending('Stock_no').find({
 	success: function(results) {
 		for(var i = 0 ; i < results.length ; i++)
 		{
-			arrangeTasks(i, results[i]);
+			func_list.push(results[i].get('Stock_no'));
 		}
+		async.mapSeries(func_list, function(stock_no, callback) {
+			Fetch(stock_no)
+			.then(function(total) { callback(null, total)},
+				function(err) {callback(err)})
+		}, function(err, results) {
+			if (err) console.error(err.toString());
+			console.log('Fetch all finished!');
+		});
 	},
 	error: function(error) {
 		console.error(error.toString());
 	}
 });
-
-function arrangeTasks(i, obj)
-{
-	setTimeout(function() {
-		Fetch(obj.get('Stock_no'));
-	}, 1000*60*3*i);
-}
